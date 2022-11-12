7299608490f18b30a5eba45da3a85edad12347c8
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-11-12 10:44:15 +0600

    Update Detail.js

--- src/components/Detail.js
+++ src/components/Detail.js
@@ -7,7 +7,7 @@ import EquipmentImage from '../assets/icons/equipment.png';
 
 const Detail = ({ exerciseDetail }) => {
   const { bodyPart, gifUrl, name, target, equipment } = exerciseDetail;
-
+  
   const extraDetail = [
     {
       icon: BodyPartImage,
@@ -22,7 +22,6 @@ const Detail = ({ exerciseDetail }) => {
       name: equipment,
     },
   ];
-
   return (
     <Stack gap="60px" sx={{ flexDirection: { lg: 'row' }, p: '20px', alignItems: 'center' }}>
       <img src={gifUrl} alt={name} loading="lazy" className="detail-image" />
@@ -50,5 +49,4 @@ const Detail = ({ exerciseDetail }) => {
     </Stack>
   );
 };
-
 export default Detail;
