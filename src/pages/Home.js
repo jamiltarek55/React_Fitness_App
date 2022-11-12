5ed3af8cc87bd34cf8c03aceb8227f4ad46d1479
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-11-12 09:57:27 +0600

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,6 @@ import HeroBanner from '../components/HeroBanner';
 const Home = () => {
   const [exercises, setExercises] = useState([]);
   const [bodyPart, setBodyPart] = useState('all');
-
   return (
     <Box>
       <HeroBanner />
