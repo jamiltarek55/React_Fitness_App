1de79103ce784a6aab916cb84f75f294be8918f0
jamiltarek55 <77003619+jamiltarek55@users.noreply.github.com>
2022-11-13 18:36:08 +0600

    Update Exercises.js

--- src/components/Exercises.js
+++ src/components/Exercises.js
@@ -24,7 +24,7 @@ const Exercises = ({ exercises, setExercises, bodyPart }) => {
     };
 
     fetchExercisesData();
-  },[setExercises]);
+  },[setExercises,bodyPart]);
 
   // Pagination
   const indexOfLastExercise = currentPage * exercisesPerPage;
